cdf490cedc0299cd999e942d53896f8232da5db4
tjfaithpro <tjbenbiz@gmail.com>
2021-09-17 10:55:49 -0700

    ini

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <div>
+        <h2>Hero</h2>
+      </div>
+
+      <div>
+        <h2>How it works</h2>
+      </div>
+
+    <div>
+      <h2>
+        Explore study group
+      </h2>
+    </div>
+ 
+
+
+    <!-- -------------------------------------------------------- -->
+
+
+    <div>
+      <h3>Why use StudyBud?</h3>
+      </div>
+
+    <div>
+      <h3>Lorem ipsum dolor, consectetur adipiscing</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+ 
+}
+</script>
